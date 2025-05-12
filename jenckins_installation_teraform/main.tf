854c543088104dd7044b4074e4b33ca8b3668f2d
Kondarao1506 <Kondarao1506@gmail.com>
2025-05-12 22:01:46 +0530

    jenckins installation

--- jenckins_installation_teraform/main.tf
+++ jenckins_installation_teraform/main.tf
@@ -1,9 +1,19 @@
-resource "aws_instance" "terra" {
+resource "aws_instance" "Master" {
 ami = data.aws_ami.ami.id
 instance_type = "t3.micro"
 vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 user_data = file("jenckins_installation.sh")
 tags = {
-    Name = "jenkins"
+    Name = "jenkins-Master"
+  }
+}
+
+resource "aws_instance" "jenkins_agent" {
+ami = data.aws_ami.ami.id
+instance_type = "t3.micro"
+vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+user_data = file("java_instalation.sh")
+tags = {
+    Name = "jenkins-agent1"
   }
 }
