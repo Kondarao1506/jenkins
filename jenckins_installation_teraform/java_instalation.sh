150926ee9cec2241f37ffa0979345295e38c7ffe
Kondarao1506 <Kondarao1506@gmail.com>
2025-05-12 23:28:00 +0530

    jenckins installation

--- jenckins_installation_teraform/java_instalation.sh
+++ jenckins_installation_teraform/java_instalation.sh
@@ -2,13 +2,13 @@
 #junckins installation
 #java installation
 
-sudo curl -o /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# sudo curl -o /etc/yum.repos.d/jenkins.repo \
+#     https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 #sudo yum upgrade
 # Add required dependencies for the jenkins package
 sudo yum install fontconfig java-21-openjdk -y
-sudo yum install jenkins -y
-sudo systemctl daemon-reload
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+# sudo yum install jenkins -y
+# sudo systemctl daemon-reload
+# sudo systemctl start jenkins
+# sudo systemctl enable jenkins
