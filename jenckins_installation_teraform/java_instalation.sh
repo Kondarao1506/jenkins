edf7a1ebd216d3bb9d7af91f4118a29ea373085f
Kondarao1506 <Kondarao1506@gmail.com>
2025-05-21 22:00:09 +0530

    jenckins installation

--- jenckins_installation_teraform/java_instalation.sh
+++ jenckins_installation_teraform/java_instalation.sh
@@ -12,3 +12,11 @@ sudo yum install fontconfig java-21-openjdk -y
 # sudo systemctl daemon-reload
 # sudo systemctl start jenkins
 # sudo systemctl enable jenkins
+sudo dnf module disable nodejs -y
+
+sudo dnf module enable nodejs:20 -y
+
+sudo dnf install nodejs -y
+
+# docker installation
+
